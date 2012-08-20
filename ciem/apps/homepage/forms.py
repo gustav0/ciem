8661a3a1b82a216a770fcc259e721c06493223f6
Gustavo Saavedra <tavito.286@gmail.com>
2012-08-19 23:42:55 -0430

    formulario logingForm implementado y agregado en index.html

--- ciem/apps/homepage/forms.py
+++ ciem/apps/homepage/forms.py
@@ -4,3 +4,11 @@ class contactForm(forms.Form):
 	email = forms.EmailField()
 	tittle = forms.CharField()
 	text = forms.CharField( widget=forms.Textarea )
+
+class registerForm(forms.Form):
+	email = forms.EmailField()
+	password = forms.CharField(widget=forms.PasswordInput)
+
+class loginForm(forms.Form):
+    email  = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- ciem/apps/homepage/views.py
+++ ciem/apps/homepage/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render_to_response
 from ciem.apps.data.models import *
 from django.template import RequestContext
-from ciem.apps.homepage.forms import contactForm
+from ciem.apps.homepage.forms import *
 
 def index(request):
 	alimentos = alimento.objects.getAll
-	ctx = { 'alimentos':alimentos }
-	return render_to_response('homepage/index.html', ctx, context_instance=RequestContext(request))
-
+	login_form = loginForm()
+	ctx = {'login_form':login_form , 'alimentos':alimentos}
+	return render_to_response('homepage/index.html', ctx,  context_instance=RequestContext(request))
+"""{ 'alimentos':alimentos },"""
+"""	alimentos = alimento.objects.getAll"""
 def about(request):
 	return render_to_response('homepage/about.html', context_instance=RequestContext(request))
 
@@ -25,4 +27,13 @@ def contact(request):
 	return render_to_response('homepage/contact.html', ctx, context_instance=RequestContext(request))
 
 def register(request):
-	return render_to_response('homepage/register.html', context_instance=RequestContext(request))
+	if request.method == "POST":
+		register_form = registerForm(request.POST)
+		if register_form.is_valid():
+			success = True
+			email = register_form.cleaned_data['email']
+			password = register_form.cleaned_data['password']
+	else:
+		register_form = registerForm()
+	ctx = {'register_form':register_form}
+	return render_to_response('homepage/register.html', ctx, context_instance=RequestContext(request))
