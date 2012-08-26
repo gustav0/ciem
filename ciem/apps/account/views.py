b331ab34fcc2cf0f2e590692c3a6e0788d73de0c
Gustavo Saavedra <tavito.286@gmail.com>
2012-08-25 23:47:28 -0430

    Fix

--- ciem/apps/account/views.py
+++ ciem/apps/account/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
-@login_required
+@login_required(login_url='/login')
 def profile(request):
 	ctx={'profile':request.user.get_profile(),}
 	return render_to_response('account/profile.html', ctx, context_instance=RequestContext(request))
-
