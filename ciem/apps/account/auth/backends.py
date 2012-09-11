9e749304adbaac187e05c714923d06bac780dc2d
Gustavo Saavedra <tavito.286@gmail.com>
2012-09-10 19:40:53 -0430

    Login funcionando bastante bien. Ordené algunas cosas.

--- ciem/apps/account/auth/backends.py
+++ ciem/apps/account/auth/backends.py
@@ -3,5 +3,4 @@ from django.contrib.auth.backends import ModelBackend
 class loginBackend(ModelBackend):
 
 	def authenticate(Self, username, password):
-		#print "Logica del login.(aprender)"
 		return super(loginBackend, Self).authenticate(username,password)
--- ciem/apps/account/views.py
+++ ciem/apps/account/views.py
@@ -1,35 +1,32 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
-from django.conf import settings
-from django.core.urlresolvers import reverse
-from django.contrib.auth import login
 from ciem.apps.account.forms import antropometricosForm, registerForm
-from django.http import HttpResponseRedirect
-
-@login_required(login_url='/login')
-def profile(request):
-	ctx={'profile':request.user.get_profile(),}
-	return render_to_response('account/profile.html', ctx, context_instance=RequestContext(request))
 
 def register(request):
 	form = registerForm(request.POST or None)
-
 	if form.is_valid():
 		user = form.save()
 		user.backend = settings.AUTHENTICATION_BACKENDS[0]
 		login(request, user)
 		return redirect(reverse('account_profile'))
-
 	ctx={'form': form,}
 	return render_to_response('account/register.html', ctx, context_instance=RequestContext(request))
 	
+@login_required(login_url='/login')
+def profile(request):
+	ctx={'profile':request.user.get_profile(),}
+	return render_to_response('account/profile.html', ctx, context_instance=RequestContext(request))
+
 @login_required(login_url='/login')
 def antropometricos(request):
 	form = antropometricosForm(request.POST or None)
 	form.user = 1;
 	if form.is_valid():
 		form.save()
-
 	ctx= {'form':form, 'profile':request.user.get_profile(),}
 	return render_to_response('account/datosAntropometricosForm.html', ctx, context_instance=RequestContext(request))	
