ff7e1d75db9a9ccc2be6c8e8d1bfbf90695e74a8
Gustavo Saavedra <tavito.286@gmail.com>
2012-09-15 13:50:42 -0430

    fix commit

--- ciem/settings.py
+++ ciem/settings.py
@@ -16,11 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ciem',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-<<<<<<< HEAD
-        'PASSWORD': '',                  # Not used with sqlite3.
-=======
         'PASSWORD': 'mundoloco',                  # Not used with sqlite3.
->>>>>>> c8c3dc1af8e81cc9ea4bf43784792879c0eae86b
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
