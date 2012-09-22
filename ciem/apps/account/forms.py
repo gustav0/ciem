e183edb72e55efd94faa8db237d1460b51ee6911
Diana Isabel Chávez <dianaichavez2@gmail.com>
2012-09-22 16:22:01 -0430

    Mas ipaq

--- ciem/apps/account/forms.py
+++ ciem/apps/account/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.forms import ModelForm
 from ciem.apps.account.models import userProfile
-from ciem.apps.account.models import datosAntropometricos
+from ciem.apps.account.models import datosAntropometricos, ipaq
 from django.contrib.auth.models import User
 
 class registerForm(UserCreationForm):
@@ -28,77 +28,19 @@ class antropometricosForm(ModelForm):
 	class Meta:
 		model = datosAntropometricos
 		
-class ipaqForm(forms.Form):
-	dias = ( ('0','1'),('1','2'),('2','3'),('3','4'),('4','5'),('5','6'),('6','7'), )
-	horas = ( ('0','-'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('10','7+'), )
-	minutos = ( ('0','-'),('1','5'),('2','10'),('3','15'),('4','30'),('5','45'), )
-
-	p2a_trabajo = forms.ChoiceField(choices = dias)
+class ipaqForm(ModelForm):
 	p2b_trabajo = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p3a_trabajo = forms.ChoiceField(choices = horas)
-	p3b_trabajo = forms.ChoiceField(choices = minutos)
-
-	p4a_trabajo = forms.ChoiceField(choices = dias)
 	p4b_trabajo = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p5a_trabajo = forms.ChoiceField(choices = horas)
-	p5b_trabajo = forms.ChoiceField(choices = minutos)	
-
-	p6a_trabajo = forms.ChoiceField(choices = dias)
 	p6b_trabajo = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p7a_trabajo = forms.ChoiceField(choices = horas)
-	p7b_trabajo = forms.ChoiceField(choices = minutos)
-	
-	p8a_transporte = forms.ChoiceField(choices = dias)
 	p8b_transporte = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p9a_transporte = forms.ChoiceField(choices = horas)
-	p9b_transporte = forms.ChoiceField(choices = minutos)
-
-	p10a_transporte = forms.ChoiceField(choices = dias)
 	p10b_transporte = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p11a_transporte = forms.ChoiceField(choices = horas)
-	p11b_transporte = forms.ChoiceField(choices = minutos)	
-	
-	p12a_transporte = forms.ChoiceField(choices = dias)
 	p12b_transporte = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p13a_transporte = forms.ChoiceField(choices = horas)
-	p13b_transporte = forms.ChoiceField(choices = minutos)	
-	
-	p12a_transporte = forms.ChoiceField(choices = dias)
-	p12b_transporte = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p13a_transporte = forms.ChoiceField(choices = horas)
-	p13b_transporte = forms.ChoiceField(choices = minutos)	
-	
-	p14a_hogar = forms.ChoiceField(choices = dias)
 	p14b_hogar = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p15a_hogar = forms.ChoiceField(choices = horas)
-	p15b_hogar = forms.ChoiceField(choices = minutos)		
-	
-	p16a_hogar = forms.ChoiceField(choices = dias)
 	p16b_hogar = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p17a_hogar = forms.ChoiceField(choices = horas)
-	p17b_hogar = forms.ChoiceField(choices = minutos)	
-	
-	p18a_hogar = forms.ChoiceField(choices = dias)
 	p18b_hogar = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p19a_hogar = forms.ChoiceField(choices = horas)
-	p19b_hogar = forms.ChoiceField(choices = minutos)	
-	
-	p20a_recreacion = forms.ChoiceField(choices = dias)
 	p20b_recreacion = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p21a_recreacion = forms.ChoiceField(choices = horas)
-	p21b_recreacion = forms.ChoiceField(choices = minutos)	
-	
-	p22a_recreacion = forms.ChoiceField(choices = dias)
 	p22b_recreacion = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p23a_recreacion = forms.ChoiceField(choices = horas)
-	p23b_recreacion = forms.ChoiceField(choices = minutos)		
-	
-	p24a_recreacion = forms.ChoiceField(choices = dias)
 	p24b_recreacion = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p25a_recreacion = forms.ChoiceField(choices = horas)
-	p25b_recreacion = forms.ChoiceField(choices = minutos)		
-	
-	p26a_sentado = forms.ChoiceField(choices = dias)
 	p26b_sentado = forms.TypedChoiceField(choices=((0, 'Si'), (1, 'No')), widget=forms.RadioSelect)
-	p27a_sentado = forms.ChoiceField(choices = horas)
-	p27b_sentado = forms.ChoiceField(choices = minutos)		
+	class Meta:
+		model = ipaq
--- ciem/apps/account/models.py
+++ ciem/apps/account/models.py
@@ -26,3 +26,74 @@ class datosAntropometricos(models.Model):
 	fecha_creacion = models.DateField(auto_now_add=True)
 
 	objects = antropometricosManager()
+
+class ipaq(models.Model):
+	dias = ( ('0','1'),('1','2'),('2','3'),('3','4'),('4','5'),('5','6'),('6','7'), )
+	horas = ( ('0','-'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('10','7+'), )
+	minutos = ( ('0','-'),('1','5'),('2','10'),('3','15'),('4','30'),('5','45'), )
+
+	p2a_trabajo = models.CharField(max_length=1, choices=dias, default='0')
+	p2b_trabajo = models.BooleanField()
+	p3a_trabajo = models.CharField(max_length=1, choices=horas, default='0')
+	p3b_trabajo = models.CharField(max_length=1, choices=minutos, default='0')
+
+
+	p4a_trabajo = models.CharField(max_length=1, choices=dias, default='0')
+	p4b_trabajo = models.BooleanField()
+	p5a_trabajo = models.CharField(max_length=1, choices=horas, default='0')
+	p5b_trabajo = models.CharField(max_length=1, choices=minutos, default='0')	
+
+	p6a_trabajo = models.CharField(max_length=1, choices=dias, default='0')
+	p6b_trabajo = models.BooleanField()
+	p7a_trabajo = models.CharField(max_length=1, choices=horas, default='0')
+	p7b_trabajo = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p8a_transporte = models.CharField(max_length=1, choices=dias, default='0')
+	p8b_transporte = models.BooleanField()
+	p9a_transporte = models.CharField(max_length=1, choices=horas, default='0')
+	p9b_transporte = models.CharField(max_length=1, choices=minutos, default='0')
+
+	p10a_transporte = models.CharField(max_length=1, choices=dias, default='0')
+	p10b_transporte =models.BooleanField()
+	p11a_transporte = models.CharField(max_length=1, choices=horas, default='0')
+	p11b_transporte = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p12a_transporte = models.CharField(max_length=1, choices=dias, default='0')
+	p12b_transporte = models.BooleanField()
+	p13a_transporte = models.CharField(max_length=1, choices=horas, default='0')
+	p13b_transporte = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p14a_hogar = models.CharField(max_length=1, choices=dias, default='0')
+	p14b_hogar =models.BooleanField()
+	p15a_hogar = models.CharField(max_length=1, choices=horas, default='0')
+	p15b_hogar = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p16a_hogar = models.CharField(max_length=1, choices=dias, default='0')
+	p16b_hogar = models.BooleanField()
+	p17a_hogar = models.CharField(max_length=1, choices=horas, default='0')
+	p17b_hogar = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p18a_hogar = models.CharField(max_length=1, choices=dias, default='0')
+	p18b_hogar = models.BooleanField()
+	p19a_hogar = models.CharField(max_length=1, choices=horas, default='0')
+	p19b_hogar = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p20a_recreacion = models.CharField(max_length=1, choices=dias, default='0')
+	p20b_recreacion = models.BooleanField()
+	p21a_recreacion = models.CharField(max_length=1, choices=horas, default='0')
+	p21b_recreacion = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p22a_recreacion = models.CharField(max_length=1, choices=dias, default='0')
+	p22b_recreacion = models.BooleanField()
+	p23a_recreacion = models.CharField(max_length=1, choices=horas, default='0')
+	p23b_recreacion = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p24a_recreacion = models.CharField(max_length=1, choices=dias, default='0')
+	p24b_recreacion = models.BooleanField()
+	p25a_recreacion = models.CharField(max_length=1, choices=horas, default='0')
+	p25b_recreacion = models.CharField(max_length=1, choices=minutos, default='0')
+	
+	p26a_sentado = models.CharField(max_length=1, choices=dias, default='0')
+	p26b_sentado = models.BooleanField()
+	p27a_sentado = models.CharField(max_length=1, choices=horas, default='0')
+	p27b_sentado = models.CharField(max_length=1, choices=minutos, default='0')
