fbd0c36aa7580f60a1c96df0938109409901e89a
Diana Isabel Chávez <dianaichavez2@gmail.com>
2012-09-22 21:22:45 -0430

    arreglos

--- ciem/apps/account/models.py
+++ ciem/apps/account/models.py
@@ -31,7 +31,7 @@ class ipaq(models.Model):
 	user = models.ForeignKey(User)
 	dias = ( ('0','1'),('1','2'),('2','3'),('3','4'),('4','5'),('5','6'),('6','7'), )
 	horas = ( ('0','-'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('10','7+'), )
-	minutos = ( ('0','-'),('1','5'),('2','10'),('3','15'),('4','30'),('5','45'), )
+	minutos = ( ('0','-'),('1','10'),('2','15'),('3','20'),('4','30'),('5','40'),('5','45') )
 
 	p2a_trabajo = models.CharField(max_length=1, choices=dias, default='0')
 	p2b_trabajo = models.BooleanField(default=True)
