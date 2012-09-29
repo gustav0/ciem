bfa463b4d0a98c80b51a0b37665fc9ee4a13fd7a
Gustavo Saavedra <tavito.286@gmail.com>
2012-09-28 22:36:23 -0430

    falto esto xD

--- ciem/apps/data/templatetags/custom_filters.py
+++ ciem/apps/data/templatetags/custom_filters.py
@@ -1,5 +1,7 @@
 from django.template import Library
 from django.db.models import Count
+from datetime import date
+
 
 register = Library()
 
@@ -8,9 +10,8 @@ def get_index(value,list):
 	mylista = value
 	for i in range(list.count()):
 		if list[i] == mylista:
-			return i+1
-	return 0
-
+			return int(i+1)
+	return int(0)
 @register.filter#DEVUELVE LISTA DEL 0 AL NUMERO INDICADO
 def sumame_uno(value):
 	return int(value)+1
@@ -20,15 +21,40 @@ def get_int(value):
 @register.filter#OBTIENE RANGO PARA UN CICLO FOR
 def get_range(value):
 	return range(value)
-
 @register.filter##OBTIENE RANGO PARA UN CICLO FOR DESDE 1
 def get_range_from1(value):
 	return range(1,value)
-
 @register.filter#REGRESA LA LISTA EN EL INDEX INDICADO
 def get_at_index(list, index):
    	return list[index]
-
 @register.filter#DIVIDIR UN STR
 def split(str,splitter):
     return str.split(splitter)
+
+
+@register.filter#calcular edad
+def calculate_age(born):
+    today = date.today()
+    try: # raised when birth date is February 29 and the current year is not a leap year
+        birthday = born.replace(year=today.year)
+    except ValueError:
+        birthday = born.replace(year=today.year, day=born.day-1)
+    if birthday > today:
+        return today.year - born.year - 1
+    else:
+        return today.year - born.year
+
+# AU= AUTH_USER, AU = ACCOUNT_USERPROFILE
+# FILTROS ESPECIFICOS PARA AU
+@register.filter#DEVUELVE FECHA DE NACIMIENTO
+def au_getFechaNaciemiento(list,mid):
+	for i in range(list.count()):
+		if list[i].id == mid:
+			return list[i].fecha_nacimiento
+	return None
+@register.filter#DEVUELVE GENERO DISPLAY
+def au_getGenero(list,mid):
+	for i in range(list.count()):
+		if list[i].id == mid:
+			return list[i].genero
+	return None
