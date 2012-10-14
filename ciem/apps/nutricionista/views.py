66734f46ec4e28408ff97f8cf555aa375f9ef190
Diana Isabel Chávez <dianaichavez2@gmail.com>
2012-10-14 15:43:48 -0430

    formset

--- ciem/apps/nutricionista/views.py
+++ ciem/apps/nutricionista/views.py
@@ -6,6 +6,21 @@ from django.http import HttpResponseRedirect
 from django.core.paginator import EmptyPage, PageNotAnInteger
 from ciem.apps.account.models import datosAntropometricos,antropometricosResultado,userProfile,ipaqResultado,ipaq,frecuenciaConsumo
 from django.contrib.auth.models import User
+from ciem.apps.data.models import *
+from django.forms.models import modelformset_factory
+
+@login_required(login_url='/login')
+def pesoAlimento(request):
+	pesosFormSet = modelformset_factory(pesoAlimento)	
+	if request.method == 'POST':
+		pesos = pesosFormSet(request.POST)
+		if pesos.is_valid():
+			pesos.save()
+	else:
+		pesos = preguntasFormSet(queryset=pesoAlimento.objects.none())
+	aliment = alimento.objects.all()
+	ctx= {'form':pesos,'alimento':aliment}
+	return render_to_response('nutricionista/pesoAlimento.html', ctx, context_instance=RequestContext(request))
 
 @login_required(login_url='/login')
 def perfilUsuarios(request):
