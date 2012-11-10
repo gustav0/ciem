776f43b57758aa352ec39ac8782647ae616ff14b
Gustavo Saavedra <tavito.286@gmail.com>
2012-11-10 15:10:35 -0430

    fix

--- ciem/staticmedia/js/propios/registroestados.js
+++ ciem/staticmedia/js/propios/registroestados.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$("#id_pais").change(function(){          
 		$pais = $(this).find("option:selected").val();
 		$otro = "25";
-		$title = "1";
+		$zulia = "24";
 		if ($pais != "VE"){
 			$("#id_venezuela option[value=" + $otro + "]").show();
 			$("#id_venezuela option[value="+$otro+"]").attr("selected",true);
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		}else{
 			$("#id_venezuela").removeAttr('disabled');
 			$("#id_venezuela option[value=" + $otro + "]").hide();
-			$("#id_venezuela option[value="+$title+"]").attr("selected",true);
+			$("#id_venezuela option[value="+$zulia+"]").attr("selected",true);
 		}
 	});
 
