774cc76d04c8b7b833ac9d5bd13632286056806d
José Ordoñez <restonjd@gmail.com>
2012-11-16 19:28:51 -0430

    tooltip text

---
+++ ciem/staticmedia/js/Adquiridos/tooltiptext.js
@@ -0,0 +1,36 @@
+this.textPreview = function(){	
+	/* CONFIG */
+		
+		xOffset = -10;
+		yOffset = -150;
+		
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result
+		
+	/* END CONFIG */
+	$("a.preview").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? this.t : "";
+		$("body").append("<p id='preview'>"+ c +"</p>");								 
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#preview").remove();
+    });	
+	$("a.preview").mousemove(function(e){
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
+// starting the script on page load
+$(document).ready(function(){
+	textPreview();
+});
